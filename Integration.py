646e43dd71ca3d983cca6ce899db95e5e073cea6
YinCheng0106 <eason010630@gmail.com>
2025-10-31 17:29:27 +0800

    feat: delete function

--- Integration.py
+++ Integration.py
@@ -1,27 +1,20 @@
-# initial environment
-# import modules
 import tkinter as tk
 import sqlite3
 
-# basic GUI 
 root = tk.Tk()
 root.title('INTEGRATION')
 root.geometry('300x350')
 
-# new label and inpu
-# student ID label and entry
 label_id = tk.Label(root, text='Student ID')
 label_id.pack(pady=(15,5))
 entry_id = tk.Entry(root, width=25)
 entry_id.pack()
 
-# student name label and entry
 label_name = tk.Label(root, text='Student Name')
 label_name.pack(pady=(10,5))
 entry_name = tk.Entry(root, width=25)
 entry_name.pack()
 
-# setting print_student function
 def print_student():
     student_id = entry_id.get()
     student_name = entry_name.get()
@@ -30,18 +23,15 @@ def print_student():
     print ('Student Name: {}'.format(student_name))
     print ('-'*30)
 
-# new a button: Print
-botton_print = tk.Button(root, text='Print', command=print_student)
-botton_print.pack(pady=15)
+button_print = tk.Button(root, text='Print', command=print_student)
+button_print.pack(pady=15)
 
-# connect to database and build environment
 conn = sqlite3.connect('Student.db')
 cursor = conn.cursor()
 
-# def a create_student()
 def create_student():
     student_id = entry_id.get()
-    student_name = entry_name.get().lower() # application layer
+    student_name = entry_name.get().lower()
 
     cursor.execute('INSERT INTO DB_student (db_student_id, db_student_name) VALUES(?,?)', (student_id, student_name))
     conn.commit()
@@ -53,17 +43,24 @@ def create_student():
 button_create = tk.Button(root, text='Create', command=create_student)
 button_create.pack(pady=20)
 
-# def a overview_student()
-# show all records in sqlite
 def overview_student():
     cursor.execute('SELECT * from DB_student')
     overview = cursor.fetchall()
     print (overview)
 
-# new botton Overview
-botton_overview = tk.Button(root, text='Overview', command=overview_student)
-botton_overview.pack(pady=25)
+button_overview = tk.Button(root, text='Overview', command=overview_student)
+button_overview.pack(pady=25)
 
-print ('hello software')
+def delete_student():
+    student_id = entry_id.get()
+    cursor.execute('SELECT * FROM DB_student WHERE db_student_id=?', (student_id,))
+    delete = cursor.fetchall()
+    cursor.execute('DELETE FROM DB_student WHERE db_student_id=?', (student_id,))
+    print ('Deleted record: {}'.format(delete))
+    conn.commit()
+    print ('-' * 30)
+    
+button_delete = tk.Button(root, text='Delete', command=delete_student)
+button_delete.pack(pady=10)
 
-root.mainloop() #must be put to the end of programming code
+root.mainloop()
